f71173f01070d02762ebe1fab8b87512e308b5a0
Benji8bit <coin8bit@hotmail.com>
2022-11-06 17:31:16 +0300

    Created TelegramBot stub with final variables and some methods

---
+++ src/main/java/telegram/TelegramBot.java
@@ -0,0 +1,42 @@
+package telegram;
+
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.helpCommand.HelpCommand;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class TelegramBot extends TelegramLongPollingCommandBot {
+    public static final String BOT_TOKEN = "5697462471:AAG3cj5OdyIr86GtdYovAhNdN8e-H5OcnAk";
+    public static final String BOT_USERNAME = "PomodoroClockBot";
+
+    public static final int WORK = 25;
+    public static final int BREAK = 5;
+
+    public TelegramBot() throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            botsApi.registerBot(this);
+            register(new StartCommand("start", "Старт"));
+            register(new HelpCommand("help", "Помощь"));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_USERNAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public void processNonCommandUpdate(Update update) {
+
+    }
+}
---
+++ src/main/java/telegram/commands/ServiceCommand.java
@@ -0,0 +1,5 @@
+package telegram.commands;
+
+public class ServiceCommand {
+
+}
