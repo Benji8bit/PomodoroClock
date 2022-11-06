e4260c455709e63cce58aec3ba5e7afe4e4ff3dc
Benji8bit <coin8bit@hotmail.com>
2022-11-07 02:11:22 +0300

    Created BeginCommand and HelpCommand

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
-public class Main {
-    public static void main(String[] args) {
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import telegram.TelegramBot;
 
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        new TelegramBot();
     }
 }
--- src/main/java/telegram/TelegramBot.java
+++ src/main/java/telegram/TelegramBot.java
@@ -1,11 +1,12 @@
 package telegram;
 
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
-import org.telegram.telegrambots.extensions.bots.commandbot.commands.helpCommand.HelpCommand;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import telegram.commands.BeginCommand;
+import telegram.commands.HelpCommand;
 
 public class TelegramBot extends TelegramLongPollingCommandBot {
     public static final String BOT_TOKEN = "5697462471:AAG3cj5OdyIr86GtdYovAhNdN8e-H5OcnAk";
@@ -18,7 +19,7 @@ public class TelegramBot extends TelegramLongPollingCommandBot {
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
         try {
             botsApi.registerBot(this);
-            register(new StartCommand("start", "Старт"));
+            register(new BeginCommand("begin", "Начать"));
             register(new HelpCommand("help", "Помощь"));
         } catch (TelegramApiException e) {
             e.printStackTrace();
